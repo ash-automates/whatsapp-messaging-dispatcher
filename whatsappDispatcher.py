81292deb0c2065dcdca4ece2c73f8b4bb60c04d7
shachem <sekki.hachem@outlook.com>
2022-09-21 14:51:43 +0100

    Load numbers to contact from contact_list.xlsx

--- whatsappDispatcher.py
+++ whatsappDispatcher.py
@@ -13,3 +13,9 @@ for filename in os.listdir("archive"):
         current_number = sheet.cell(row=i, column=2).value
         if not current_number == None and current_number not in archived_nums:
             archived_nums.append(current_number)
+
+for i in range(2, numbers_sheet.max_row):
+    num = numbers_sheet.cell(row=i, column=2).value
+    was_contacted = numbers_sheet.cell(row=i, column=5).value
+    if not num == None and num not in archived_nums and not was_contacted == "x":
+        numbers_to_contact.append(num)
